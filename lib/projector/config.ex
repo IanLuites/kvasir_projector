d487d5a80869af5f52221af6c2a06bac8436e48e
Ian Luites <IanLuites@users.noreply.github.com>
2020-01-15 00:50:31 +0100

    Add projector config.

---
+++ lib/projector/config.ex
@@ -0,0 +1,23 @@
+defmodule Kvasir.Projector.Config do
+  def state(opts) do
+    case opts[:cache] || settings()[:cache] do
+      {mod, opt} -> {mod, opt}
+      mod -> {mod, []}
+    end
+  end
+
+  def state!(opts), do: state(opts) || raise("state not set for projector or in config.")
+
+  defp settings, do: Application.get_env(:kvasir, :projector, [])
+
+  def projections(opts) do
+    case opts[:projections] || opts[:projection] do
+      nil -> []
+      projections when is_list(projections) -> projections
+      projection -> [projection]
+    end
+  end
+
+  def projections!(opts),
+    do: projections(opts) || raise("projections not set for projector or in config.")
+end
