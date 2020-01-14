4f07dad2873fff6b39892bb613b29708dab92341
Ian Luites <IanLuites@users.noreply.github.com>
2020-01-15 00:50:11 +0100

    Add base projector.

--- lib/projector.ex
+++ lib/projector.ex
@@ -1,58 +1,72 @@
 defmodule Kvasir.Projector do
-  @moduledoc """
+  @moduledoc ~S"""
   Documentation for Kvasir.Projector.
   """
 
-  defmodule Stateful do
-    @callback apply(Kvasir.Event.t(), state :: any) :: {:ok, any} | {:error, atom}
-  end
-
-  defmodule Stateless do
-    @callback apply(Kvasir.Event.t()) :: :ok | {:error, atom}
-  end
-
   defmacro __using__(opts \\ []) do
-    _behavior =
-      if opts[:stateful] do
-        quote do
-          @behaviour Kvasir.Projector.Stateful
-        end
-      else
-        quote do
-          @behaviour Kvasir.Projector.Stateless
-        end
-      end
-
-    client = opts[:client]
-    topic = opts[:topic]
-    projections = opts[:projections] || []
+    ### Actual Logic ###
+    source = opts[:source] || raise "Need to pass the Kvasir EventSource."
+    topic = opts[:topic] || raise "Need to pass the Kafka topic."
+    projections = Kvasir.Projector.Config.projections!(opts)
+    {state, state_opts} = Macro.expand(Kvasir.Projector.Config.state!(opts), __CALLER__)
+    {app, version, hex, hexdocs, code_source} = Kvasir.Util.documentation(__CALLER__)
 
     # Disabled environments
-    if Mix.env() in (opts[:disable] || []) do
-      nil
-    else
-      Enum.reduce(
-        projections,
-        nil,
-        fn projection, acc ->
-          projection = Macro.expand(projection, __CALLER__)
+    unless Mix.env() in (opts[:disable] || []) do
+      quote do
+        require unquote(source)
 
-          quote do
-            unquote(acc)
+        @source unquote(source)
+        @topic unquote(topic)
+        @state unquote({state, Macro.escape(state_opts)})
+        @projections unquote(projections)
 
-            defmodule unquote(Module.concat(projection, Projector)) do
-              @moduledoc false
-              use Kvasir.Subscriber,
-                client: unquote(client),
-                topic: unquote(topic)
+        @doc false
+        @spec child_spec(opts :: Keyword.t()) :: Supervisor.child_sec()
+        def child_spec(_opts \\ []), do: unquote(__MODULE__).child_spec(__MODULE__)
 
-              @doc false
-              @impl Kvasir.Subscriber
-              def handle_event(event), do: unquote(projection).apply(event)
-            end
-          end
-        end
-      )
+        @doc false
+        @spec start_link(opts :: Keyword.t()) :: GenServer.on_start()
+        def start_link(_opts \\ []), do: unquote(__MODULE__).start_link(__MODULE__)
+
+        @doc false
+        @spec __projector__(atom) :: term
+        def __projector__(:config),
+          do: %{
+            projections: @projections,
+            projector: __MODULE__,
+            source: @source,
+            state: @state,
+            topic: @topic
+          }
+
+        def __projector__(:projections), do: @projections
+        def __projector__(:projector), do: __MODULE__
+        def __projector__(:source), do: @source
+        def __projector__(:state), do: @state
+        def __projector__(:topic), do: @topic
+        def __projector__(:app), do: {unquote(app), unquote(version)}
+        def __projector__(:hex), do: unquote(hex)
+        def __projector__(:hexdocs), do: unquote(hexdocs)
+        def __projector__(:code_source), do: unquote(code_source)
+      end
     end
+  end
+
+  def child_spec(projector) do
+    %{
+      id: projector,
+      start: {Kvasir.Projector, :start_link, [projector]}
+    }
+  end
+
+  def start_link(projector) do
+    children =
+      :projections
+      |> projector.__projector__()
+      |> Enum.map(& &1.child_spec(projector: projector, projection: &1))
+      |> Enum.reject(&(&1 == :no_start))
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: projector)
   end
 end
